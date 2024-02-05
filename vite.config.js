98fa8941dcea3dc241abec0c0365fe4ca094f598
masarom <romansanmaria@gmail.com>
2024-02-05 18:09:57 +0100

    modify package.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://masarom.github.io/rick-morty-vite',
+  base:'/rick-morty-vite/',
   css: {
     preprocessorOptions: {
       scss: {
